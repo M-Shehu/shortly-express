b008d8e840311c5fc1b6f07a81fe1a544411cb4d
M-Shehu <magnanimus.shehu@gmail.com>
2019-03-15 21:25:58 -0700

    Implement createSession

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,53 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+    if (typeof req.cookies === 'object') {
+        if (Object.keys(req.cookies).length === 0) {
+            return models.Sessions.create()
+            .then((result) => {
+                return models.Sessions.get({id: result.insertId})
+            })
+            .then((results) => {
+                return req.session = results;
+            })
+            .then(() => {
+                return res.cookies = {'shortlyid' : {'value': req.session.hash}};
+            })
+            .then(() => {
+                next();
+            })
+            .catch(() => {
+                console.log('Oops!');
+            })
+        } else {
+            return models.Sessions.get({hash: req.cookies.shortlyid})
+            .then((results) => {
+                req.session = results;
+            })
+            .then(() => {
+                next();
+            })
+            .catch(() => {
+                return models.Sessions.create()
+                .then((result) => {
+                    return models.Sessions.get({id: result.insertId})
+                })
+                .then((result) => {
+                    console.log(result);
+                    return res.cookies = {'shortlyid' : result.hash};
+                })
+                .then(() => {
+                    next();
+                })
+                .catch(() => {
+                    console.log('Oops!');
+                })
+            })
+            
+        }
+    }
+
+    next();
 };
 
 /************************************************************/
